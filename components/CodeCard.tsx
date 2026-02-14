f78f7e659c1db9f49c7aecf4220b272756505dd6
Harshita Yadav <seemayadavanuj123@gmail.com>
2026-02-14 11:35:07 +0530

    Update CodeCard.tsx

--- components/CodeCard.tsx
+++ components/CodeCard.tsx
@@ -63,7 +63,7 @@ export default function CodeCard() {
                         <span className="text-green-400 font-bold">npm run intro</span>
                     </div>
                     <div className="mt-1 text-white font-bold">
-                        + version@21.3.18
+                        + Harshita@21.3.18
                     </div>
                     <div className="text-white mt-1 leading-relaxed">
                         Robotics Enthusiast • Open Source Contributor • Full-Stack
